6f45782b5d3cc41a90ac038102faae7d2c85f90d
FRANCIELLE DE ABREU <francielle@alexandria.solar>
2023-10-06 17:19:39 -0300

    feat: Address model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_06_194410) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_201658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "cep"
+    t.string "street"
+    t.string "neighborhood"
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -26,6 +36,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_06_194410) do
     t.string "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "address_id", null: false
+    t.index ["address_id"], name: "index_users_on_address_id"
   end
 
+  add_foreign_key "users", "addresses"
 end
