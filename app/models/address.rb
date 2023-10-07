8af3b33b9efdecbea3aab3720623fe4d0125ed84
FRANCIELLE DE ABREU <francielle@alexandria.solar>
2023-10-07 15:48:41 -0300

    feat: create user and address done

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,23 @@
 class Address < ApplicationRecord
   has_many :users
+
+  def self.create_from_cep(cep)
+    address_data = ViaCepService.fetch_address(cep).parsed_response
+
+    puts "Address Data: #{address_data.inspect}"
+
+    address = find_or_create_by(cep: address_data['cep'].gsub('-', '')) do |addr|
+      addr.street = address_data['logradouro']
+      addr.city = address_data['localidade']
+      addr.state = address_data['uf']
+      addr.neighborhood = address_data['bairro']
+    end
+
+    if address.persisted?
+      address
+    else
+      Rails.logger.error "Failed to save address: #{address.errors.full_messages.join(', ')}"
+      nil
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
   belongs_to :address
+  has_secure_password
+  before_validation :set_address_from_cep
+
+  private
+
+  def set_address_from_cep
+    created_address = Address.create_from_cep(cep)
+    if created_address
+      self.address = created_address
+    else
+      errors.add(:cep, 'Failed to process cep')
+      throw(:abort)
+    end
+  rescue StandardError => e
+    Rails.logger.error "Failed to create address: #{e.message}"
+    errors.add(:cep, 'Failed to process cep')
+    throw(:abort)
+  end
 end
